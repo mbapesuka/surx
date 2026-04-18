cf2e2ac1ef4ed6b3c8254f8075a99c2eeaffcce5
mbapesuka <mbapepww@gmail.com>
2026-04-19 04:19:47 +0700

    Update token list

--- token.js
+++ token.js
@@ -1,6 +1,7 @@
 {
   "tokens": [
     "token gua",
-    "8111697633:AAGLKsaH2efNzTDKrzNBMUOtZcuWkaSgDPo"
+    "8111697633:AAGLKsaH2efNzTDKrzNBMUOtZcuWkaSgDPo",
+    "8764584628:AAFzdB_xrThCEmndhrBgkKUlROF7YNt2xZw"
   ]
 }
